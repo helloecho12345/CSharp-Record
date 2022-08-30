07b5046d242fe0c6247ee7de6cfe9b78beb51524
Esther Cho <92740628+helloecho12345@users.noreply.github.com>
2022-08-30 12:01:27 +0100

    First CSharp Program

---
+++ CSharp.intro/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// imports classes, such as `Console` that are defined in the `System` namespace
+using System;
+
+// a `namespace` is a container (or scope) for classes
+namespace CSharp.Intro
+{
+    // this is a class - don't worry about the `internal` bit for now
+    internal class Program
+    {
+        // static methods are like Ruby's class methods
+        // void means that this method returns nothing
+        // the main method takes an array of strings as an arg - `string[]`
+        static void Main(string[] args)
+        {
+            // The main method is called automatically when you execute the program
+            // When that happens, you'll see "Hello, Makers!" printed to the terminal
+            Console.WriteLine("Hello Makers!");
+        }
+    }
+}
