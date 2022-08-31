56821d49b55be1f9e6a53e7155f80a4d0c391789
Esther Cho <92740628+helloecho12345@users.noreply.github.com>
2022-08-31 12:38:27 +0100

    string interpolation with correct syntax

---
+++ PriceDisplay/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PriceDisplay {
+  class PriceDisplay {
+
+    static void Main(string[] args){
+      string item = "Coffee Machine";
+      double price = 15.99;
+      new PriceDisplay().DisplayItemPrice(item, price);
+    }
+    public void DisplayItemPrice(string item, double price){
+      Console.WriteLine("The price of the item {0} is {1}.", item, price);
+    }
+  }
+
+}
