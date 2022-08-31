1068907254d393157eac23487935f3b01a069792
Esther Cho <92740628+helloecho12345@users.noreply.github.com>
2022-08-31 13:26:30 +0100

    LongestName exercise completed

---
+++ LongestName/Program.cs
@@ -0,0 +1,24 @@
+﻿class LongestName
+{
+  public string GetLongest(string[] names)
+  {
+    string longest = "";
+    foreach (string name in names)
+    {
+      if (longest.Length < name)
+      {
+        longest = name;
+      }
+    }
+    return longest;
+  }
+}
+
+
+// Can you spot the problem with this code?
+  // Can't run comparison between integer and string
+// Whether or not you found the issue, now run this code in irb. Call the longest_name function by entering puts longest_name(people).
+// Did the bug happen when creating the function, or only when calling the function?
+  // only when calling the function
+// In C#, would the behaviour with such a bug be the same, or different? Why?
+  // In C#, the behaviour would be different since it is a compiling language and wouldn't let you compile whereas with Ruby which has an interpreter, the code will run with an error.
