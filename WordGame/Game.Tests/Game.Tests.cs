712d98dd75ec932ee783abd628303c3698056ae2
Esther Cho <92740628+helloecho12345@users.noreply.github.com>
2022-09-02 14:51:06 +0100

    answered questions

--- WordGame/Game.Tests/Game.Tests.cs
+++ WordGame/Game.Tests/Game.Tests.cs
@@ -11,3 +11,8 @@ namespace Game.Tests {
     }
   }
 }
+
+// What is the type of the value on the "right" of the assertion ("B_____")? 
+  // the type is a string
+// What then needs to be the type returned by GetWordToGuess()?
+  // the returned type needs to be a string
--- WordGame/Game/Game.cs
+++ WordGame/Game/Game.cs
@@ -13,3 +13,8 @@
     }
   }
 }
+
+
+
+// Write the class Game with the minimal required code to make this test pass. Don't worry too much about any game logic. For now, your only goal is to make this test pass.
+  // Hardcoded return "B_____"; to make test pass.
